30a61fb7e1f8de6d63a06a3dcaab966cca8422d3
Darshan Chouhan <cdarshan123789@gmail.com>
2024-11-28 12:17:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PkgDataForm from './PkgDataForm';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LoginPage/> */}
+      <PkgDataForm />
+      
+     
+    </div>
+  );
+}
+
+export default App;
